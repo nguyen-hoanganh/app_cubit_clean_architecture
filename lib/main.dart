bd6bca1ea9d41c62b12d7d8111c648b17bdc7eaf
anhnh <anhnh.ous@mcredit.com.vn>
2023-11-23 09:28:19 +0700

    [HA] add typedefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:app_cubit_clean_architecture/core/res/colours.dart';
 import 'package:app_cubit_clean_architecture/core/res/fonts.dart';
 import 'package:app_cubit_clean_architecture/core/services/route.dart';
-import 'package:app_cubit_clean_architecture/src/on_boarding/presentation/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,8 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
         useMaterial3: true,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme:const  AppBarTheme(
-          color:  Colors.transparent,
+        appBarTheme: const AppBarTheme(
+          color: Colors.transparent,
         ),
         fontFamily: Fonts.poppins,
       ),
