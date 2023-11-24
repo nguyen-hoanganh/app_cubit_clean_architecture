658dadabb8a634161c564713fc3a1b54b538665b
anhnh <anhnh.ous@mcredit.com.vn>
2023-11-24 22:21:20 +0700

    [HA] OnBoarding Screen data source implimentation

--- lib/src/on_boarding/datasources/on_boarding_local_data_source.dart
+++ lib/src/on_boarding/datasources/on_boarding_local_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:app_cubit_clean_architecture/core/errors/exceptions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class OnBoardingLocalDataSource {
@@ -5,23 +6,31 @@ abstract class OnBoardingLocalDataSource {
 
   Future<void> cacheFirstTimer();
 
-  Future<void> checkIfUserIsFirstTimer();
+  Future<bool> checkIfUserIsFirstTimer();
 }
 
 const kFirstTimerKey = 'first_timer';
 
 class OnBoardingLocalDataSrcImpl extends OnBoardingLocalDataSource {
-  const OnBoardingLocalDataSrcImpl(this._pref);
+  const OnBoardingLocalDataSrcImpl(this._prefs);
 
-  final SharedPreferences _pref;
+  final SharedPreferences _prefs;
 
   @override
-  Future<void> cacheFirstTimer() {
-    throw UnimplementedError();
+  Future<void> cacheFirstTimer() async {
+    try {
+      await _prefs.setBool(kFirstTimerKey, false);
+    } catch (e) {
+      throw CacheException(message: e.toString());
+    }
   }
 
   @override
-  Future<void> checkIfUserIsFirstTimer() {
-    throw UnimplementedError();
+  Future<bool> checkIfUserIsFirstTimer() async {
+    try {
+      return _prefs.getBool(kFirstTimerKey) ?? true;
+    } catch(e){
+      throw CacheException(message: e.toString());
+    }
   }
 }
