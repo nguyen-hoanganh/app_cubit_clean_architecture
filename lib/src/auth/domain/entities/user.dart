f495137fa39221c7f204e99b16710e80d4bd1fd2
anhnh <anhnh.ous@mcredit.com.vn>
2023-11-29 17:11:41 +0700

    [HA] auth models

--- lib/src/auth/domain/entities/user.dart
+++ lib/src/auth/domain/entities/user.dart
@@ -4,27 +4,27 @@ class LocalUser extends Equatable {
   const LocalUser({
     required this.uid,
     required this.email,
-    this.profilePic,
-    this.bio,
     required this.points,
     required this.fullName,
-    required this.groupId,
-    required this.enrolledCourseIds,
-    required this.following,
-    required this.follower,
+    this.groupIds = const [],
+    this.enrolledCourseIds = const [],
+    this.following = const [],
+    this.followers = const [],
+    this.profilePic,
+    this.bio,
   });
 
   const LocalUser.empty()
       : this(
           uid: '',
           email: '',
-          points: '',
+          points: 0,
           fullName: '',
           profilePic: '',
           bio: '',
-          groupId: const [],
+          groupIds: const [],
           enrolledCourseIds: const [],
-          follower: const [],
+          followers: const [],
           following: const [],
         );
 
@@ -32,12 +32,12 @@ class LocalUser extends Equatable {
   final String email;
   final String? profilePic;
   final String? bio;
-  final String points;
+  final int points;
   final String fullName;
-  final List<String> groupId;
+  final List<String> groupIds;
   final List<String> enrolledCourseIds;
   final List<String> following;
-  final List<String> follower;
+  final List<String> followers;
 
   @override
   List<Object?> get props => [uid, email];
